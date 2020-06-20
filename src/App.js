aae2cf3df7ff588c7670d7b23557716426781e89
AceIsHuman <menoman1995@yahoo.com>
2020-06-20 14:06:49 -0500

    add title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { styled } from '@material-ui/core/styles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper maxWidth='lg'>
+      <Title>Conway's Game of Life</Title>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled('div')({
+  backgroundColor: '#202020',
+  height: '100vh',
+  width: '100%',
+  position: 'absolute',
+  display: 'flex',
+  justifyContent: 'center',
+});
+
+const Title = styled('h1')({
+  color: '#EEEEEE',
+});
 
 export default App;
