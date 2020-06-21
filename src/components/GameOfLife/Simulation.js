8cdd4d300d0b9a54b6e3df4fcac3d1e21b67f8c7
AceIsHuman <menoman1995@yahoo.com>
2020-06-20 22:21:51 -0500

    begin structuring elements

---
+++ src/components/GameOfLife/Simulation.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { styled } from '@material-ui/core';
+import Grid from './Grid';
+import Controls from './Controls';
+
+function Simulation() {
+  return (
+    <Container>
+      <Generation>Generation: #</Generation>
+      <Grid />
+      <Controls />
+    </Container>
+  );
+}
+
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const Generation = styled('h3')({
+  color: '#EEEEEE',
+});
+
+export default Simulation;
