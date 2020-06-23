a9ac6bd0249899f306e9d71ee4223563eab760b4
AceIsHuman <menoman1995@yahoo.com>
2020-06-22 23:38:48 -0500

    begin creating Cell class

---
+++ src/components/GameOfLife/Grid/helpers/Cell.js
@@ -0,0 +1,19 @@
+export class Cell {
+  constuctor(x, y, isAlive) {
+    this.x = x;
+    this.y = y;
+    this.isAlive = isAlive;
+  }
+
+  nextGenState(matrix) {
+    for (let a = -1; a < 2; a++) {
+      for (let b = -1; b < 2; b++) {
+        if (a === 0 && b === 0) {
+          continue;
+        }
+        const cell = matrix[this.x + a][this.y + b];
+        console.log(cell);
+      }
+    }
+  }
+}
