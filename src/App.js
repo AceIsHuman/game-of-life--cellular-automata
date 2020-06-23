57eabdf01af57a545f08e00c7bf3d0e5987dc3c9
AceIsHuman <menoman1995@yahoo.com>
2020-06-22 22:10:30 -0500

    update Wrapper style, add About component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import GameOfLife from './components/GameOfLife/GameOfLife';
+import About from './components/About';
 
 function App() {
   return (
     <Wrapper maxWidth='lg'>
       <Title>Conway's Game of Life</Title>
       <GameOfLife />
+      <About />
     </Wrapper>
   );
 }
 
 const Wrapper = styled('div')({
   backgroundColor: '#202020',
-  height: '100vh',
   width: '100%',
-  position: 'absolute',
   display: 'flex',
   alignItems: 'center',
-  flexDirection: 'column'
+  flexDirection: 'column',
+  padding: '0 10rem',
+  boxSizing: 'border-box',
 });
 
 const Title = styled('h1')({
