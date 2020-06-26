3514dd635775a19c27410a4f802da0e23c268ed2
AceIsHuman <menoman1995@yahoo.com>
2020-06-25 22:19:02 -0500

    create playSimulation() and getNextGen()

--- src/components/GameOfLife/GameOfLife.js
+++ src/components/GameOfLife/GameOfLife.js
@@ -1,16 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { styled } from '@material-ui/core/styles';
 
 import Simulation from './Simulation';
 import Presets from './Presets/Presets';
 import Rules from './Rules';
+import { fillGrid } from './helpers';
+import { Cell } from './helpers/Cell';
 
 function GameOfLife() {
   const [currentGen, setCurrentGen] = useState([]);
+  const [genCount, setGenCount] = useState(1);
+  const [playing, setPlaying] = useState(false);
+  const canvas = document.getElementById('grid');
+
+  const getNextGen = async (currentGen) => {
+    const rows = new Array(currentGen.length).fill([]);
+    const next = rows.map(i => new Array(currentGen.length).fill(0));
+    for (let y = 0; y < currentGen.length; y++) {
+      for (let x = 0; x < currentGen.length; x++) {
+        const cell = currentGen[y][x];
+        next[y][x] = new Cell(x, y, cell.nextGenState(currentGen));
+      }
+    }
+
+    return next;
+  };
+
+  const playSimulation = useCallback(() => {
+    return setTimeout(async () => {
+      if (playing) {
+        const next = await getNextGen(currentGen);
+        setGenCount(genCount + 1);
+        setCurrentGen(next);
+        fillGrid(canvas, next);
+      }
+    }, 1000);
+  }, [genCount, currentGen, playing, canvas]);
+
+  useEffect(() => {
+    if (playing) {
+      let simulation = playSimulation();
+
+      return function () {
+        clearInterval(simulation);
+      };
+    }
+  }, [playing, currentGen, playSimulation, canvas]);
 
   return (
     <Container>
-      <Simulation currentGen={currentGen} setCurrentGen={setCurrentGen} />
+      <Simulation
+        currentGen={currentGen}
+        setCurrentGen={setCurrentGen}
+        setPlaying={setPlaying}
+        playSimulation={playSimulation}
+        genCount={genCount}
+      />
       <Presets setCurrentGen={setCurrentGen} />
       <Rules />
     </Container>
