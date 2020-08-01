ec10a18522458c6bada7ce82c96b290dd65236f7
AceIsHuman <menoman1995@yahoo.com>
2020-08-01 16:48:26 -0500

    update About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { styled } from '@material-ui/core/styles';
 import { SubTitle } from './Reusables/';
+import ExamplePatterns from '../assets/example_patterns';
 
 function About() {
   return (
     <Container>
-      <SubTitle>About this Algorithm:</SubTitle>
-      <Text>
-        Aliquip qui esse non ad non. Exercitation fugiat ex ea deserunt
-        voluptate eiusmod consectetur cupidatat laborum. Amet do irure laboris
-        consectetur consequat. Ullamco nostrud excepteur ipsum nulla cillum
-        tempor culpa deserunt. Dolor ex dolore id nulla. Elit nulla laborum
-        commodo et.
-      </Text>
+      <SubTitle style={{ color: '#000000', marginBottom: 0 }}>
+        About this Algorithm:
+      </SubTitle>
+      <Content>
+        <Text>
+          Check out this classic computer science program replicated using the
+          React framework! John Conway's "Game of Life" simulates
+          <i>cellular automaton</i> by following a few set of rules for cells on a
+          grid. A matrix is created to represent each Cell, it's current state,
+          and location on the grid. By using a <code>Cell</code> class, each Cell
+          predicts its next generation state by checking all of its adjacent
+          neighbors current state. The total number of alive neighbors is then
+          placed against the rules of life. This uses double buffering to perform the update against a seperate grid before applying the changes to the canvas.
+        </Text>
+        <ExamplePatterns />
+      </Content>
     </Container>
   );
 }
@@ -21,10 +30,20 @@ const Container = styled('div')({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
+  backgroundColor: '#FFFFFF',
+  color: '#333333',
+  margin: '3rem 0',
+  padding: '0.5rem',
+  borderRadius: '0.5rem',
 });
 
 const Text = styled('p')({
+  maxWidth: '50%',
+  fontSize: '1.2rem',
+  fontWeight: '500',
   marginBottom: '1rem',
 });
+
+const Content = styled('div')({display: 'flex', justifyContent: 'space-around'})
 
 export default About;
