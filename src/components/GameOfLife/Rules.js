89c7be710480df96dfc5f8b174ae6ce5530c430f
AceIsHuman <menoman1995@yahoo.com>
2020-08-01 16:07:07 -0500

    update wording of rules

--- src/components/GameOfLife/Rules.js
+++ src/components/GameOfLife/Rules.js
@@ -8,19 +8,14 @@ function Rules() {
     <Container>
       <SubTitle>Rules:</SubTitle>
       <ul>
-        <li>Alive cells are represented by colored squares on the grid.</li>
         <li>
-          If an alive cell has either 2 or 3 live neighbors, the cell will
-          continue to live in the next generation.
+          An alive cell will continue to live if it has 2 or 3 live neighbors.
         </li>
         <li>
-          If an alive cell does not meet that criteria, it will die in the next
-          generation.
+          Likewise, and alive cell will die if it has less than 2, or more than
+          3 live neighbors.
         </li>
-        <li>
-          The only way for a dead cell to come alive, is if it has exactly three
-          neighbor cells that are alive.
-        </li>
+        <li>A cell will come alive when it has exactly 3 neighbors.</li>
       </ul>
     </Container>
   );
