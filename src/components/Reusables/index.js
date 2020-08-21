45f1a9596b07c5355d9385188ffb8d7982d4cb69
AceIsHuman <menoman1995@yahoo.com>
2020-08-21 16:25:50 -0500

    remove right margin at md breakpoint for vertical container

--- src/components/Reusables/index.js
+++ src/components/Reusables/index.js
@@ -1,11 +1,14 @@
 import { styled } from '@material-ui/core';
 
-export const VerticalContainer = styled('div')({
+export const VerticalContainer = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   marginRight: '1rem',
-});
+  [theme.breakpoints.down('md')]: {
+    marginRight: '0',
+  },
+}));
 
 export const SubTitle = styled('h3')({
   color: '#EEEEEE',
