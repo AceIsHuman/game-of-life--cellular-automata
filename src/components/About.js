f95efe50bb46c7bc993b8b5a6a2d9d1e3e32b519
AceIsHuman <menoman1995@yahoo.com>
2020-08-21 15:00:06 -0500

    add md breakpoint for content container and text

--- src/components/About.js
+++ src/components/About.js
@@ -13,12 +13,14 @@ function About() {
         <Text>
           Check out this classic computer science program replicated using the
           React framework! John Conway's "Game of Life" simulates
-          <i>cellular automaton</i> by following a few set of rules for cells on a
-          grid. A matrix is created to represent each Cell, it's current state,
-          and location on the grid. By using a <code>Cell</code> class, each Cell
-          predicts its next generation state by checking all of its adjacent
-          neighbors current state. The total number of alive neighbors is then
-          placed against the rules of life. This uses double buffering to perform the update against a seperate grid before applying the changes to the canvas.
+          <i>cellular automaton</i> by following a few set of rules for cells on
+          a grid. A matrix is created to represent each Cell, it's current
+          state, and location on the grid. By using a <code>Cell</code> class,
+          each Cell predicts its next generation state by checking all of its
+          adjacent neighbors current state. The total number of alive neighbors
+          is then placed against the rules of life. This uses double buffering
+          to perform the update against a seperate grid before applying the
+          changes to the canvas.
         </Text>
         <ExamplePatterns />
       </Content>
@@ -37,13 +39,22 @@ const Container = styled('div')({
   borderRadius: '0.5rem',
 });
 
-const Text = styled('p')({
+const Text = styled('p')(({ theme }) => ({
   maxWidth: '50%',
   fontSize: '1.2rem',
   fontWeight: '500',
   marginBottom: '1rem',
-});
+  [theme.breakpoints.down('md')]: {
+    maxWidth: '100%',
+  },
+}));
 
-const Content = styled('div')({display: 'flex', justifyContent: 'space-around'})
+const Content = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'space-around',
+  [theme.breakpoints.down('md')]: {
+    flexDirection: 'column',
+  },
+}));
 
 export default About;
