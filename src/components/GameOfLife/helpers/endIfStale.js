153f10868e32d039c3fb00e6f2ef3fcb80cff509
AceIsHuman <menoman1995@yahoo.com>
2020-08-24 23:16:15 -0500

    add endIfStale helper function

--- src/components/GameOfLife/helpers/endIfStale.js
+++ src/components/GameOfLife/helpers/endIfStale.js
@@ -1,5 +1,5 @@
-function matrixToInt(matrix) {
-  const matrixString = '';
+export function matrixToInt(matrix) {
+  let matrixString = '';
   for (let row of matrix) {
     for (let cell of row) {
       matrixString += cell.isAlive ? '1' : '0';
@@ -7,4 +7,17 @@ function matrixToInt(matrix) {
   }
 
   return parseInt(matrixString, 2);
+}
+
+// Recieves an array of matrix integers representing their state.
+// If last two are same, this is a still life.
+// End the simulation
+export function endIfStale(history, setPlaying) {
+  const lastIndex = history.length - 1;
+  if (history.length >= 2) {
+    const [previous, current] = [history[lastIndex - 1], history[lastIndex]];
+    if (!(current ^ previous)) {
+      setPlaying(false);
+    }
+  }
 }
