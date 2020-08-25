947a3421b0c5c7cbd8817638a9d8066b4c64c40b
AceIsHuman <menoman1995@yahoo.com>
2020-08-24 23:03:28 -0500

    refactor for useGridState

--- src/components/GameOfLife/GameOfLife.js
+++ src/components/GameOfLife/GameOfLife.js
@@ -5,41 +5,22 @@ import Simulation from './Simulation';
 import Presets from './Presets/Presets';
 import Rules from './Rules';
 import { fillGrid } from './helpers';
-import { Cell } from './helpers/Cell';
+import useGridState from './helpers/useGridState';
 
 function GameOfLife() {
-  const [currentGen, setCurrentGen] = useState([]);
-  const [genCount, setGenCount] = useState(1);
+  const [grid, setInitialGen, setNextToCurr, setCellState] = useGridState()
   const [playing, setPlaying] = useState(false);
-  const canvas = document.getElementById('grid');
   const [speed, setSpeed] = useState(1);
-
-  const getNextGen = async (currentGen) => {
-    // create empyt array to populate next gen data
-    const rows = new Array(currentGen.length).fill([]);
-    const next = rows.map((i) => new Array(currentGen.length).fill(0));
-
-    // determine cell state and create new Cells on next
-    for (let y = 0; y < currentGen.length; y++) {
-      for (let x = 0; x < currentGen.length; x++) {
-        const cell = currentGen[y][x];
-        next[y][x] = new Cell(x, y, cell.nextGenState(currentGen));
-      }
-    }
-
-    return next;
-  };
+  const canvas = document.getElementById('grid');
 
   const playSimulation = useCallback(() => {
     return setTimeout(async () => {
       if (playing) {
-        const next = await getNextGen(currentGen);
-        setGenCount(genCount + 1);
-        setCurrentGen(next);
-        fillGrid(canvas, next);
+        fillGrid(canvas, grid.nextGen);
+        setNextToCurr();
       }
     }, speed * 500);
-  }, [genCount, currentGen, playing, canvas, speed]);
+  }, [grid, setNextToCurr, playing, canvas, speed]);
 
   useEffect(() => {
     if (playing) {
@@ -48,24 +29,22 @@ function GameOfLife() {
       return function () {
         clearInterval(simulation);
       };
-    }
-  }, [playing, currentGen, playSimulation, canvas]);
+    } 
+  }, [playing, grid, playSimulation, canvas]);
 
   return (
     <Container>
       <Simulation
-        currentGen={currentGen}
-        setCurrentGen={setCurrentGen}
+        grid={grid}
+        setInitialGen={setInitialGen}
+        setCellState={setCellState}
         playing={playing}
         setPlaying={setPlaying}
-        genCount={genCount}
-        setGenCount={setGenCount}
         setSpeed={setSpeed}
       />
       <Presets
-        setCurrentGen={setCurrentGen}
+        setInitialGen={setInitialGen}
         playing={playing}
-        setGenCount={setGenCount}
       />
       <Rules />
     </Container>
