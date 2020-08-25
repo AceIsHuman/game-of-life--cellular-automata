c0b5addc7928476c4c4ef077314c3c16406a6b2a
AceIsHuman <menoman1995@yahoo.com>
2020-08-24 23:07:55 -0500

    refactor to use grid.currentGen and setCellState

--- src/components/GameOfLife/Grid.js
+++ src/components/GameOfLife/Grid.js
@@ -7,32 +7,26 @@ function Grid(props) {
   useEffect(() => {
     const canvas = document.getElementById('grid');
     generateGrid(canvas);
-    fillGrid(canvas, props.currentGen);
-  }, [props.currentGen]);
+    fillGrid(canvas, props.grid.currentGen);
+  }, [props.grid.currentGen]);
 
   return (
     <canvas
       id='grid'
       onClick={(e) => {
         if (props.playing) return;
-        switchCellState(e, props.currentGen, props.setCurrentGen, canvas);
+        switchCellState(e, props.setCellState, canvas);
       }}
       style={{ cursor: props.playing ? null : 'pointer' }}
     ></canvas>
   );
 }
 
-function switchCellState(e, currentGen, setGenState, canvas) {
+function switchCellState(e, setCellState, canvas) {
   // Get x and y from click event
   const x = Math.floor((e.pageX - canvas.offsetLeft) / cellSize);
   const y = Math.floor((e.pageY - canvas.offsetTop) / cellSize);
-
-  const cell = currentGen[y][x];
-
-  const updatedGen = [...currentGen];
-  updatedGen[y][x].isAlive = !cell.isAlive;
-
-  setGenState(updatedGen);
+  setCellState(x,y);
 }
 
 export default Grid;
