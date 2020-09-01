da13657c18ba0fd4e8e3104f6f33c988d3b698ba
AceIsHuman <menoman1995@yahoo.com>
2020-09-01 16:16:20 -0500

    show staleMessage on occurance

--- src/components/GameOfLife/Simulation.js
+++ src/components/GameOfLife/Simulation.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { VerticalContainer, SubTitle } from '../Reusables/';
 import Grid from './Grid';
 import Controls from './Controls';
+import { Typography } from '@material-ui/core';
 
 function Simulation(props) {
   return (
     <VerticalContainer>
-      <SubTitle size='h3'>Generation: {props.grid.genCount}</SubTitle>
+      <SubTitle>Generation: {props.grid.genCount}</SubTitle>
+      {props.staleMessage && <Typography color='error' variant='p'>{props.staleMessage}</Typography>}
       <Grid
         grid={props.grid}
         setCellState={props.setCellState}
